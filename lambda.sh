0ac1415e852363b485776fe5c0a575616dae011a
James Livermont <jlivermont@docutap.com>
2018-10-30 11:08:03 -0500

    Added Lambda test & upload script

--- lambda.sh
+++ lambda.sh
@@ -7,10 +7,10 @@ zip lambda.zip all_letters.py
 rm all_letters.py
 
 set +e
-aws lambda delete-function --function-name all-letters-validator
+aws lambda delete-function --function-name all-letters-validator --region us-west-2
 set -e
 
-aws lambda create-function --region us-west-2 --profile jlivermont \
+aws lambda create-function --region us-west-2 \
   --function-name all-letters-validator --zip-file fileb://lambda.zip \
   --role "arn:aws:iam::846025239765:role/jlivermont-api-role" \
   --handler all_letters.all_letters_validator_lambda --runtime python3.6
@@ -18,13 +18,13 @@ aws lambda create-function --region us-west-2 --profile jlivermont \
 rm lambda.zip
 
 aws lambda invoke --invocation-type RequestResponse --function-name all-letters-validator \
-  --region us-west-2 --log-type Tail --profile jlivermont \
+  --region us-west-2 --log-type Tail \
   --payload '{"string": "abcdefg"}' output.txt
 
 grep 'false' output.txt
 
 aws lambda invoke --invocation-type RequestResponse --function-name all-letters-validator \
-  --region us-west-2 --log-type Tail --profile jlivermont \
+  --region us-west-2 --log-type Tail \
   --payload '{"string": "abcdefghijklmnopqrstuvwxyz"}' output.txt
 
 grep 'true' output.txt
